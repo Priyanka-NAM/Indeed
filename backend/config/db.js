72f837e6302dc014d3836ccda579996e486b03db
saisrinivaslakkakula <38246768+saisrinivaslakkakula@users.noreply.github.com>
2021-10-23 15:39:43 -0700

    initial Commit/ Boiler plate code

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose =  require('mongoose')
+const env = require('dotenv')
+const connectDB = async () =>{
+    try { 
+        // mongoose connect always returns a promise// hence await is required
+        const con = await mongoose.connect('mongodb+srv://team7:mypassword@indeeddb.awj0c.mongodb.net/test?authSource=admin&replicaSet=atlas-rzo35r-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+            {useUnifiedTopology:true,useNewUrlParser:true}
+            )
+       console.log(`Connected to DB ${con.connection.host}`) 
+    } catch (error) {
+        console.log(`Connection Failed! ${error}`) 
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
