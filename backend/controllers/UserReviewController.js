830163c7e2c28f5327fc24618f49374f041369b7
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-10-31 01:03:50 -0700

    Added Reviews APIS

--- backend/controllers/UserReviewController.js
+++ backend/controllers/UserReviewController.js
@@ -1,7 +1,7 @@
 const Reviews = require('../Models/ReviewsModel')
 
-const postUserReview = async (req, res) => {
-    const {overallRating, reviewSummary, yourReview, pros, cons, isApproved, isFeatured, interviewPreparation} = req.body
+exports.postUserReview = async (req, res) => {
+    const {overallRating, reviewSummary, yourReview, pros, cons, isApproved, isFeatured, interviewPreparation, user} = req.body
     const usersReview = await Reviews.create({
         overallRating,
         reviewSummary,
@@ -10,18 +10,97 @@ const postUserReview = async (req, res) => {
         cons,
         isApproved,
         isFeatured,
-        interviewPreparation
+        interviewPreparation,
+        user,
     })
-    const data = {
-        _id : usersReview._id,
-        reviewSummary: usersReview.reviewSummary
+    res.status(200).send(usersReview)
+}
+exports.findReviewById = async(req, res, next, id) => {
+    try{
+    const review = await Reviews.findById(id);
+    req.review = review;
+    if(!review){
+        return res.status(400).json({
+            error: error
+        });
     }
-    res.status(200).send(data)
+    next();
+  }
+    catch(error){
+        return res.status(400).json({
+          error: error
+        });
+    }
+  };
+
+exports.getUserReviews = async (req, res) => {
+    try{
+        const review = await Reviews.find({user: req.body.id}).populate('user');
+        req.review = review;
+        if(!review){
+            return res.status(400).json({
+                error: error
+            });
+        }
+        res.send(review);
+      }
+        catch(error){
+            return res.status(400).json({
+              error: error
+            });
+        }
 }
 
-const getUserReviews = async (req, res) => {
-    const reviews = await Reviews.find({})
-    res.status(200).send(reviews)
+exports.getEmployerReviews = async (req, res) => {
+    try{
+        const review = await Reviews.find({employer: req.body.id}).populate('employer');
+        req.review = review;
+        if(!review){
+            return res.status(400).json({
+                error: error
+            });
+        }
+        res.send(review);
+      }
+        catch(error){
+            return res.status(400).json({
+              error: error
+            });
+        }
 }
 
-module.exports = {postUserReview, getUserReviews}
+exports.getAllReviews = async (req, res) => {
+    try{
+        const review = await Reviews.find({});
+        req.review = review;
+        if(!review){
+            return res.status(400).json({
+                error: error
+            });
+        }
+        res.send(review);
+      }
+        catch(error){
+            return res.status(400).json({
+              error: error
+            });
+        }
+}
+
+exports.UpdateReviewStatus = async (req, res) => {
+    console.log(req.review);
+    try{
+        const review = await Reviews.findOneAndUpdate({ _id: req.review._id }, { isApproved: req.body.isApproved })
+        if(!review){
+            return res.status(400).json({
+                error: error
+            });
+        }
+        res.send(review);
+      }
+        catch(error){
+            return res.status(400).json({
+              error: error
+            });
+        }
+}
--- backend/routes/CompanyRoutes.js
+++ backend/routes/CompanyRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { getUserReviews } = require('../controllers/UserReviewController')
-const { postUserReview } = require('../controllers/UserReviewController')
+const { getUserReviews, findReviewById, postUserReview, getAllReviews, UpdateReviewStatus} = require('../controllers/UserReviewController')
 const userSalary = require('../controllers/UserSalaryController')
-const router = express.Router()
+const router = express.Router();
 
+router.param('reviewId', findReviewById);
 router.post('/user-review', postUserReview)
 router.get('/user-review', getUserReviews)
 router.post('/user-salary', userSalary)
+router.get('/reviews', getAllReviews)
+router.put('/review/:reviewId', UpdateReviewStatus)
 
 module.exports = router
