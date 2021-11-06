e04ae9b74152058e7e4f178ee9a59d5bc2ab2970
Manjushree Berike Rajanna <manju_br@Manjushrees-MacBook-Pro.local>
2021-11-06 13:13:54 -0700

    Front end bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './components/LandingComponent/langingPage';
+import { ThemeProvider } from "@material-ui/core";
+import Theme from "./Utils/Theme";
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={Theme}>
+      <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
         </Routes>
-      </Router>      
+      </Router> 
+      </div>
+    </ThemeProvider>
+          
     </div>
   );
 }
--- frontend/src/components/LandingComponent/langingPage.js
+++ frontend/src/components/LandingComponent/langingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../Header/header';
+import Home from '../Home/Home';
 
 function Landing() {
     return (
         <div>
+
             <Header />
+            <Home />
             Hello World!
         </div>
     );
