220557dcedb2184c554b3ae104c742467523be37
Priyanka <alivelumanjulokapriyanka.nareddy@sjsu.edu>
2021-11-06 12:47:40 -0700

    updated Jobcontroller

--- backend/controllers/JobController.js
+++ backend/controllers/JobController.js
@@ -18,14 +18,9 @@ const createJob = async (req, res) => {
   } = req.body; // get the data from request body which is in json and put it in variables called user and password
   console.log("requestis", req);
   const jobExists = await Jobs.findOne({ jobId });
-  //res.send(req.body)
   if (jobExists) {
     res.status("400").send("Error");
-    //throw new Error ("Employer Already exists")
   } else {
-    //console.log("asas")
-    //res.status("200").json(req.body)
-
     const job = await Jobs.create({
       jobId,
       jobTitle,
@@ -58,11 +53,57 @@ const createJob = async (req, res) => {
   }
 };
 
-
 /* 
 @ Update
 /indeed/employer/update-job
 Employer Update Job Route
  */
+const updateJob = async (req, res) => {
+  const {
+    jobId,
+    jobTitle,
+    employerID,
+    companyName,
+    jobLocation,
+    jobType,
+    isRemote,
+    salary,
+    jobDescription,
+  } = req.body; // get the data from request body which is in json and put it in variables called user and password
+  console.log("requestis", req);
+  const jobExists = await Jobs.findOne({ jobId });
+  if (!jobExists) {
+    res.status("400").send("Error");
+  } else {
+    const job = await Jobs.updateOne({
+      jobId,
+      jobTitle,
+      employerID,
+      companyName,
+      jobLocation,
+      jobType,
+      isRemote,
+      salary,
+      jobDescription,
+    });
 
-module.exports = createJob;
+    if (job) {
+      console.log("Updated!");
+      res.status(201).json({
+        jobId,
+        jobTitle,
+        employerID,
+        companyName,
+        jobLocation,
+        jobType,
+        isRemote,
+        salary,
+        jobDescription,
+      });
+    } else {
+      res.status("400");
+      throw new Error("400 Bad Request: Please try again later. ");
+    }
+  }
+};
+module.exports = { createJob, updateJob };
