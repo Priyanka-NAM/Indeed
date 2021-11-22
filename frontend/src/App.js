321f30598aa16e43f44965aa14ccf97c9d46d7ef
Priyanka <alivelumanjulokapriyanka.nareddy@sjsu.edu>
2021-11-21 16:09:26 -0800

    Updated Employee SignUp Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import Theme from "./Utils/Theme";
-import Landing from './components/LandingComponent/langingPage';
-import Signup from './components/Signup/Signup';
+import Landing from "./components/LandingComponent/langingPage";
+import Signup from "./components/Signup/Signup";
+import EmployerSignup from "./components/Employer/EmployerSignUp";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <ThemeProvider theme={Theme}>
-      <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/Signup" component={Signup} />
-        </Switch>
-      </Router> 
-      </div>
-    </ThemeProvider>
-          
+        <div className='App'>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Landing} />
+              <Route path='/Signup' component={Signup} />
+              <Route path='/postjob' component={EmployerSignup} />
+            </Switch>
+          </Router>
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
