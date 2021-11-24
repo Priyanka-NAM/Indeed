cb70b258d0027756d10bd5024bcd8b2563e2a6fe
MANJUSHREEBR <brmanjushree92@gmail.com>
2021-11-23 23:24:36 -0800

    Added Company homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from "@material-ui/core";
-import Theme from "./Utils/Theme";
+import Theme from "./Utils/theme";
 import Landing from './components/Landing/LangingPage';
 import Signup from './components/Signup/Signup';
+import Company from './components/Company/Company';
 import { Login } from './components/Login/Login';
-
 function App() {
   return (
     <div className="App">
@@ -16,6 +16,7 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
+          <Route exact path="/company/:pathname" component={Company} />
         </Switch>
       </Router> 
       </div>
--- frontend/src/Redux/Stores.js
+++ frontend/src/Redux/Stores.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, createStore, compose } from "redux";
 import thunk from "redux-thunk"
 import { signUpReducer } from "./Reducers/SignupReducer";
 import { loginReducer } from "./Reducers/LoginReducer";
+import { CompanyDetailsReducer } from "./Reducers/CompanyReducer";
 
 const rootReducer = combineReducers({
     signup: signUpReducer,
-    login: loginReducer
+    login: loginReducer,
+    companyDetails: CompanyDetailsReducer,
 })
 
 const createComposer  = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
