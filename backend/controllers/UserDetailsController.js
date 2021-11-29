4e18c7b18470b03817f9adb9c343fd79ce42f792
Nishanth09 <inumpudinishanth96@gmail.com>
2021-11-28 21:32:06 -0800

    saved jobs controller added

--- backend/controllers/UserDetailsController.js
+++ backend/controllers/UserDetailsController.js
@@ -1,8 +1,31 @@
 const User = require("../Models/UserModel");
 
-const updateUserSavedJobs = (req, res) => {
-    console.log(req.body)
-    return res.status(200)
+const updateUserSavedJobs = async (req, res) => {
+    const {userId, jobId} = req.body
+    console.log(userId, jobId)
+    if (userId) {
+        const jobExists = await User.find({savedJobs : jobId})
+        console.log("jobexists : ", jobExists)
+        const user = await User.findOneAndUpdate({_id: userId}, {$push: {"savedJobs": jobId}}, {new:true})
+        if (user) {
+            res.status(200).send("added job to saved jobs")
+        } else {
+            res.status(500).send("database error")
+        }
+    }
+} 
+
+const deleteUserSavedJobs = async (req, res) => {
+    const {userId, jobId} = req.body
+    console.log(userId, jobId)
+    if (userId) {
+        const user = await User.findOneAndUpdate({_id: userId}, {$pull: {"savedJobs": jobId}}, {new:true})
+        if (user) {
+            res.status(200).send("removed from saved jobs")
+        } else {
+            res.status(500).send("database error")
+        }
+    }
 }
 
-module.exports = { updateUserSavedJobs }
+module.exports = { updateUserSavedJobs, deleteUserSavedJobs }
