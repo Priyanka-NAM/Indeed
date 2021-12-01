8193b737f06a036d806a1a25b2da2563d3b231a6
Priyanka <alivelumanjulokapriyanka.nareddy@sjsu.edu>
2021-12-01 00:04:29 -0800

    Added Report Functionality in backend and fixed bugs

--- backend/routes/JobRoutes.js
+++ backend/routes/JobRoutes.js
@@ -4,11 +4,15 @@ const {
   createJob,
   updateJob,
   getAllJobs,
-  getJobApplicants
+  getJobApplicants,
+  jobApplications,
+  eachJobApplications,
 } = require("../controllers/JobController");
 
 router.post("/post-job", createJob);
 router.post("/update-job", updateJob);
 router.get("/jobs-posted/:employerID", getAllJobs);
 router.get("/job-applicants/:id", getJobApplicants);
+router.get("/jobapplications/aggregate/:id", jobApplications);
+router.get("/eachjobapplications/aggregate/:id", eachJobApplications);
 module.exports = router;
