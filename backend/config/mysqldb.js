056bca4cdbcbbf2538be47e1a8c9dbc796b5dad8
Nishanth09 <inumpudinishanth96@gmail.com>
2021-12-01 21:32:37 -0800

    mysql connection updated

--- backend/config/mysqldb.js
+++ backend/config/mysqldb.js
@@ -1,9 +1,19 @@
 const mysql = require("mysql");
 
+// const pool = mysql.createPool({
+//   host: "indeed.cxz6a21rgeds.us-east-1.rds.amazonaws.com",
+//   user: "admin",
+//   password: "12345678",
+//   database: "indeed",
+//   port: 3306,
+//   debug: false,
+//   connectionLimit: 100,
+//   multipleStatements: true,
+// });
 const pool = mysql.createPool({
-  host: "indeed.cxz6a21rgeds.us-east-1.rds.amazonaws.com",
+  host: "database-1.cgpwh4cefk7y.us-east-2.rds.amazonaws.com",
   user: "admin",
-  password: "12345678",
+  password: "dontaskmypassword",
   database: "indeed",
   port: 3306,
   debug: false,
