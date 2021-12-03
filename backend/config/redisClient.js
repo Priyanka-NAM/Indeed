776f26876650910fccaaff563cee7ee607d1d056
Nishanth09 <inumpudinishanth96@gmail.com>
2021-12-03 15:09:26 -0800

    Update redisClient.js

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -5,17 +5,17 @@ console.log(redisHost)
 console.log(redisPort)
 const redisClient = redis.createClient(redisPort, redisHost);
 
-// redisClient.on('error', (err) => console.log(err))
+redisClient.on('error', (err) => console.log(err))
 
-// redisClient.connect()
+redisClient.connect()
 
-// redisClient.on("connect", (err) =>  {
-//     if(err){
-//         console.log("Error while connecting to Redis server");
-//     }
-//     else {
-//         console.log("Redis Server Connected");
-//     }
-// });
+redisClient.on("connect", (err) =>  {
+    if(err){
+        console.log("Error while connecting to Redis server");
+    }
+    else {
+        console.log("Redis Server Connected");
+    }
+});
 
 module.exports = redisClient;
