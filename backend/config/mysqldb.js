f972fb800e99f15ecdfafe456c3fdfce973ade81
Priyanka-NAM <87109108+Priyanka-NAM@users.noreply.github.com>
2021-12-03 19:09:43 -0800

    Update mysqldb.js
    Changed config

--- backend/config/mysqldb.js
+++ backend/config/mysqldb.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "database-1.cgpwh4cefk7y.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "dontaskmypassword",
-  database: "indeed",
+  host: process.env.SQLHOST,
+  user: process.env.USER,
+  password: process.env.PASSSWORD,
+  database: process.env.DATABASEINDEED,
   port: 3306,
   debug: false,
   connectionLimit: 100,
